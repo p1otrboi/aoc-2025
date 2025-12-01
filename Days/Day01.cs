516c66be1b96eb1ff16069f958ff91d9d15ed328
p1otrboi <102698651+p1otrboi@users.noreply.github.com>
2025-12-01 10:03:36 +0100

    day 1

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -5,7 +5,61 @@ public class Day01 : BaseDay
     private readonly string[] _lines;
     public Day01() => _lines = File.ReadAllLines(InputFilePath);
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        int dial = 50;
+        int zeroes = 0;
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (string line in _lines)
+        {
+            int clicks = int.Parse(line.AsSpan(1));
+            int operation = line[0] == 'L' ? -clicks : clicks;
+
+            dial = (dial + operation) % 100;
+        
+            if (dial < 0) 
+                dial += 100;
+
+            if (dial == 0)
+                zeroes++;
+        }
+        return new(zeroes.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int dial = 50;
+        int zeroes = 0;
+
+        foreach (string line in _lines)
+        {
+            char dir = line[0];
+            int clicks = int.Parse(line.AsSpan(1));
+            int start = dial;
+            int first;
+
+            if (dir == 'R')
+            {
+                first = (100 - start) % 100;
+                if (first == 0) 
+                    first = 100;
+                dial = (start + clicks) % 100;
+            }
+            else
+            {
+                first = start % 100;
+                if (first == 0) 
+                    first = 100;
+                dial = (start - clicks) % 100;
+                if (dial < 0) 
+                    dial += 100;
+            }
+
+            if (clicks >= first)
+                zeroes += 1 + (clicks - first) / 100;
+        }
+        return new(zeroes.ToString());
+    }
 }
+
+
