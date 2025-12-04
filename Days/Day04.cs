396c9d48291e8147428bed3ff530f118cdea70a7
p1otrboi <102698651+p1otrboi@users.noreply.github.com>
2025-12-04 16:25:27 +0100

    Day 04 wip

---
+++ Days/Day04.cs
@@ -0,0 +1,56 @@
+﻿namespace aoc_2025.Days;
+
+public sealed class Day04 : BaseDay
+{
+    private readonly char[][] _matrix;
+
+    public Day04()
+    {
+        _matrix = [.. File.ReadAllLines(InputFilePath).Select(l => l.ToCharArray())];
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int accessCount = 0;
+
+        for (int i = 0; i < _matrix.Length; i++)
+        {
+            for (int j = 0; j < _matrix[i].Length; j++)
+            {
+                if (_matrix[i][j] == '@' && CheckAccess(i, j))
+                {
+                    Console.WriteLine($"Access found at ({i}, {j})");
+                    accessCount++;
+                }
+            }
+        }
+
+        return new(accessCount.ToString());
+    }
+
+    public bool CheckAccess(int pointX, int pointY)
+    {
+        int neighbors = 0;
+
+        // check array bounds
+        int left = Math.Max(pointX - 1, 0);
+        int right = Math.Min(pointX + 1, _matrix[0].Length - 1);
+        int top = Math.Max(pointY - 1, 0);
+        int bottom = Math.Min(pointY + 1, _matrix.Length - 1);
+
+        for (int y = top; y <= bottom; y++)
+        {
+            for (int x = left; x <= right; x++)
+            {
+                if (_matrix[y][x] == '@')
+                    neighbors++;
+            }
+        }
+        return neighbors < 4;
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("Del2");
+    }
+}
